4722e00e208141cea48ac81e61179f4b818f2bee
Joel Samuel <joel.samuel@bellevuecollege.edu>
2025-05-16 22:19:54 -0700

    team_bios.js
    create and add js file, link js to html

---
+++ team_project/js/team_bios.js
@@ -0,0 +1,32 @@
+const bios = {
+  wilber: {
+    text: "Add Text Here",
+    style: "wilber.css",
+  },
+  joel: {
+    text: "Add Text Here",
+    style: "joel.css",
+  },
+  devin: {
+    text: "Add Text Here.",
+    style: "devin.css",
+  },
+  aaron: {
+    text: "Add Text Here",
+    style: "aaron.css",
+  }
+};
+
+document.getElementById('member-select').addEventListener('change', function () {
+  const selected = this.value;
+  const bioSection = document.getElementById('member-bio');
+  const styleLink = document.getElementById('member-style');
+
+  if (bios[selected]) {
+    bioSection.textContent = bios[selected].text;
+    styleLink.href = `./css/${bios[selected].style}`;
+  } else {
+    bioSection.textContent = '';
+    styleLink.href = './css/project_details.css'; 
+  }
+});
