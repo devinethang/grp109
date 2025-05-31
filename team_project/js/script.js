db56455a23f1b7d5e7458ee46cad5143880baafc
simplywilber <simplywilber@gmail.com>
2025-05-31 14:11:04 -0700

    Replace hardcoded footer into a dynamic footer

--- team_project/js/script.js
+++ team_project/js/script.js
@@ -23,4 +23,22 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Footer block
+let n = new Date();
+let year = n.getFullYear();
 
+const footer = document.getElementById("footer");
+
+footer.innerHTML = `
+  <div class="footer-container">
+    <div>
+      <p>&copy; ${year} Honey Hive. All rights Reserved.</p>
+    </div>
+    <div>
+      <a href="http://validator.w3.org/check?uri=https://devinethang.github.io/grp109/team_project/index.html" target="_blank">
+        <img src="http://www.w3.org/Icons/valid-xhtml10" alt="HTML Validator" height="44" width="100">
+      </a>
+      <a href="https://jigsaw.w3.org/css-validator/validator?uri=https://devinethang.github.io/grp109/team_project/index.html" target="_blank">
+        <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Validator" height="44" width="100">
+      </a>
+    </div>
+  </div>`;
